2b69a2ec3d29d23e6a4675123770fce6a8bf0d7d
Adrian Sims <adriansims62781@gmail.com>
2021-04-23 11:18:23 +0200

    Add project files.

---
+++ IV1/X.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace IV1
+{
+    public interface I1
+    {
+        void Print();
+    }
+
+    public class X1 : I1
+    {
+        public void Print()
+        {
+            Trace.WriteLine("X1");
+        }
+    }
+
+    public class AlternativeX1 : I1
+    {
+        public void Print()
+        {
+            Trace.WriteLine("Alternative X1");
+        }
+    }
+
+    public enum CreateType
+    {
+        Normal,
+        Alternative,
+        External
+    }
+
+    public class XFactory
+    {
+        private static CreateType createType = CreateType.Normal;
+        private static Type altI1Type;
+
+        public static I1 Create()
+        {
+            switch (createType)
+            {
+                case CreateType.Normal:
+                    return new X1();
+                case CreateType.Alternative:
+                    return new AlternativeX1();
+                case CreateType.External:
+                    return (I1)Activator.CreateInstance(altI1Type);
+                default:
+                    throw new ArgumentException("Bad create type");
+            }
+        }
+
+        internal static void SetAlt(CreateType v)
+        {
+            createType = v;
+        }
+
+        internal static void ProvideAlt(Type type)
+        {
+            altI1Type = type;
+        }
+    }
+}
---
+++ IV1Tests/XTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using IV1;
+
+namespace IV1.Tests
+{
+    public class ExternalI1 : I1
+    {
+        public void Print()
+        {
+            Trace.WriteLine("External I1");
+        }
+    }
+
+    [TestClass()]
+    public class XTests
+    {
+        [TestMethod()]
+        public void SomethingTest()
+        {
+            IV1.XFactory.SetAlt(CreateType.Normal);
+            var x1 = XFactory.Create();
+            x1.Print();
+
+            IV1.XFactory.SetAlt(CreateType.Alternative);
+            var x2 = XFactory.Create();
+            x2.Print();
+
+            IV1.XFactory.SetAlt(CreateType.External);
+            IV1.XFactory.ProvideAlt(typeof(ExternalI1));
+            var x3 = XFactory.Create();
+            x3.Print();
+
+        }
+    }
+}
